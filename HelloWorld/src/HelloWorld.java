1ff93030557860e392a08738c105120bbe82d632
Maculo <S02590993@ccc.ccofc.edu>
2023-09-01 16:11:51 -0600

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/****************************************************************************
+ *  This is a Hello World program, it writes "Hello World!" to the console. *
+ *  Written by: Izzy Maculo                                                 *
+ *  Date: 9/1/23                                                            *
+ ***************************************************************************/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello world, one line comment
+		System.out.println("Hello World!");
+		
+		int num1 = 66;
+		int num2 = 12;
+		int feet = num1 / num2;
+		int inches = num1 % num2;
+		
+		System.out.println("The item is " + feet + " feet and " + inches + " inches.");
+		
+	}
+
+}
