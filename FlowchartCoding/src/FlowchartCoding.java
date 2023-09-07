af96d5fed550f8964c45179426f3b9bb4a2a6fab
Maculo <S02590993@ccc.ccofc.edu>
2023-09-07 16:05:03 -0600

    New flowchart assignment, Flowchart 1 and 2 from a couple weeks ago in code

---
+++ FlowchartCoding/src/FlowchartCoding.java
@@ -0,0 +1,40 @@
+
+/************************************************************************************************************
+ *  This is a practice program, it runs through previous flowcharts done in this class but with real code.  *
+ *  Written by: Izzy Maculo                                                                                 *
+ *  Date: 9/7/23                                                                                            *
+ ***********************************************************************************************************/
+
+import java.util.Scanner;
+
+public class FlowchartCoding {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int creds;
+		float cost = 0;
+		String activityFee;
+		
+		System.out.print("How many credits are you taking this semester?: ");
+		creds = input.nextInt();
+		
+		if (creds > 12) {
+			cost = cost + 250;
+		}
+		else {
+			System.out.print("Do you want to purchase the activity fee for the recreation center? (Y/N): ");
+			activityFee = input.next();
+			activityFee = activityFee.toUpperCase();
+			
+			if (activityFee.equals("Y")) {
+				cost = cost + 250;
+			}
+		}
+		
+		cost = cost + (creds * 580) + 650;
+		System.out.printf("The total cost of this semester is " + "$%.2f", cost);
+
+	}
+}
