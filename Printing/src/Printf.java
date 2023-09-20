73f27ca341a41706e666d0f4c5ccc13fe38fb881
Maculo <S02590993@ccc.ccofc.edu>
2023-09-20 16:51:56 -0600

    I feel like a god

--- Printing/src/Printf.java
+++ Printing/src/Printf.java
@@ -6,7 +6,7 @@ public class Printf {
 //		String formattedStr = String.format("\nGrand Total: %,.2f", 1000000.00);
 //		System.out.println(formattedStr);
 		
-		int count = 0;
+//		int count = 0;
 		
 //		for (int i = 0; i < 10; i++) {
 //			for (int j = 0; j < 10; j++) {
@@ -18,11 +18,39 @@ public class Printf {
 		
 //		System.out.println("\n\n\n");
 		
+		int spacing = 5;
+		int length = (spacing*10)+11;
+		
 		for (int k = 0; k < 10; k++) {
+			doLine(length);
+			System.out.print("|");
 			for (int l = 0; l < 10; l++) {
-				System.out.printf("%5s", (k+1)*(l+1));
+				centerAlign(spacing, k, l);
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
+		doLine(length);
+	}
+	
+	public static void centerAlign(int space, int k, int l) {
+		String value = Integer.toString((k+1)*(l+1));
+		space = space - value.length();
+		
+		for (int i = 0; i < (space/2+(space%2)); i++) {
+			System.out.print(" ");
+		}
+		System.out.print((k+1)*(l+1));
+		for (int i = 0; i < ((space/2)); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("|");
+			
+		}
+	
+	public static void doLine(int num) {
+		for (int i = 0; i < num; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
 	}
 }
