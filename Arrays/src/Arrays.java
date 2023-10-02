c62b2c7ddc1201a305505e9aaee72a12783412f6
Maculo <S02590993@ccc.ccofc.edu>
2023-10-02 16:11:04 -0600

    Array practice

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Arrays {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("How many words to put in the array?: ");
+		int numStrings = input.nextInt();
+		
+		String[]strings = new String[numStrings];
+		
+		for (int i = 0; i < numStrings; i++) {
+			System.out.print("What is word #" + (i+1) + "?: ");
+			strings[i] = input.next();
+		}
+		
+		for (int j = 0; j < numStrings; j++) {
+			System.out.print(strings[j] + " ");
+		}
+		
+		int[]strLengths = new int[numStrings];
+		
+		for (int k = 0; k < numStrings; k++) {
+			strLengths[k] = strings[k].length();
+		}
+		
+		System.out.println();
+		
+		for (int l = 0; l < numStrings; l++) {
+			System.out.print(strLengths[l] + " ");
+		}
+		
+	}
+
+}
