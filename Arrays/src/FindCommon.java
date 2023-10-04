2e111c154cf30bc7dd369d2c5569a13a7fb07863
Maculo <S02590993@ccc.ccofc.edu>
2023-10-04 16:40:18 -0600

    More stuff

--- Arrays/src/FindCommon.java
+++ Arrays/src/FindCommon.java
@@ -8,22 +8,30 @@ import java.util.Arrays;
 public class FindCommon {
 
 	public static void main(String[] args) {
-		int[] nums1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int[] nums2 = {9, 8, 5, 6, 7, 83, 54, 104, 53424, 9};
+		int[] nums1 = {9, 9};
+		int[] nums2 = {9};
 		
 		int[] common = findCommon(nums1, nums2);
 		outputArray(common);
 	}
 	
 	public static int[] findCommon(int[] nums1, int[] nums2) {
-		int[] commonFull = new int[nums1.length + nums2.length];
+		if (nums1.length > nums2.length) {
+			int[] bigger = Arrays.copyOf(nums1, nums1.length);
+			int[] smaller = Arrays.copyOf(nums2, nums2.length);
+		}
+		else {
+			int[] smaller = Arrays.copyOf(nums1, nums1.length);
+			int[] bigger = Arrays.copyOf(nums2, nums2.length);
+		}
+		int[] commonFull = new int[smaller.length];
 		int count = 0;
 		for (int i = 0; i < nums1.length; i++) {
 			for (int j = 0; j < nums2.length; j++) {
-				System.out.println(Arrays.binarySearch(commonFull, nums1[i]));
-				if (nums1[i] == nums2[j] && Arrays.binarySearch(commonFull, nums1[i]) > 0) {
+				if (nums1[i] == nums2[j] && Arrays.binarySearch(commonFull, nums1[i]) < -1) {
 					commonFull[count] = nums1[i];
 					count++;
+					break;
 				}
 			}
 		}
