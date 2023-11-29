3cde8870053096838b216cf9ef8c006876d667f6
Maculo <S02590993@ccc.ccofc.edu>
2023-11-29 16:24:47 -0700

    Last minute edit

--- ClassesCheckSheet/src/FileReader.java
+++ ClassesCheckSheet/src/FileReader.java
@@ -20,24 +20,25 @@ public class FileReader {
 		if (input.hasNextLine()) {
 			String line = input.nextLine();
 			String[] tokens = line.split(",");
+			Course course = new Course("", 0, 0, 0);
 			
 			if (tokens[0].equals("Course")) {
-				Course course = new Course(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
+				course = new Course(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
 			}
 			if (tokens[0].equals("Full Remote")) {
-				Course course = new FullRemoteCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), tokens[5]);
+				course = new FullRemoteCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), tokens[5]);
 			}
 			if (tokens[0].equals("In-Person")) {
-				Course course = new InPersonCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]));
+				course = new InPersonCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]));
 			}
 			if (tokens[0].equals("Real-Time Remote")) {
-				Course course = new InPersonCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]));
+				course = new InPersonCourse(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]));
 			}
+			return course;
 		}
 		else {
 			return null;
 		}
-		return null;
 	}
 	
 	public List<Course> readCoursesFromFile() throws FileNotFoundException {
